0bb871c698e2f6bd5573698bf1d63ebf18da50a1
A-DRAMANE <abdoulcommedramane@gmail.com>
2021-04-26 17:37:45 +0000

    add bases features and mains functions

--- src/components/CalcuFonction.js
+++ src/components/CalcuFonction.js
@@ -1,11 +1,20 @@
 import React from 'react'
 
-function CalcuFonction() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+export async function Calcul(nom1,nom2) {
+    const calculResponse = [];
 
-export default CalcuFonction
+    fetch(`https://love-calculator.p.rapidapi.com/getPercentage?fname=${nom1}&sname=${nom2}`, {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "d24ff0ee3amshc0fc56cc0a62b3dp176859jsncaddd0907e0e",
+		"x-rapidapi-host": "love-calculator.p.rapidapi.com"
+        }
+    })
+    .then(response => response.json())
+    .then(response => {
+        console.log('a',response);
+    })
+    .catch(err => {
+        console.error(err);
+    });
+}
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,45 @@
-import React from 'react'
+import React,{useState} from 'react'
+import {Calcul} from './CalcuFonction'
 
-export function Calcul(name1, name2) {
-    
+export function Home() {
+    const [tonNom, setTonNom] = useState('');
+    const [sonNom, setSonNom] = useState('');
+
+    const handleCalcule = async (tonNom,sonNom) =>{
+        const respons = await Calcul(tonNom,sonNom)
+        console.log(respons);
+    }
+    return (
+        <div className="container">
+            <h1>LOVE CALCULATOR</h1>
+            <h2>Trouve ton % d'AMOUR</h2>
+            <div className="form-contain">
+                <div className="form">
+                    <div className="form-row">
+                        <h3></h3>
+                        <input
+                            type="text"
+                            placeholder="Votre nom"
+                            value={tonNom}
+                            onChange={e => setTonNom(e.target.value)}
+                        />
+                    </div>
+                    <div className="form-row midel"></div>
+                    <div className="form-row">
+                        <h3></h3>
+                        <input
+                            type="text"
+                            placeholder="Son nom"
+                            value={sonNom}
+                            onChange={e => setSonNom(e.target.value)}
+                        />
+                    </div>
+                </div>
+                <button onClick={handleCalcule}>CALCULATE LOVE %</button>
+            </div>
+            <h1>{}</h1>
+        </div>
+    )
 }
+
+export default Home
